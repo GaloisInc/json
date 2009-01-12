4d60b168a1e8359ca370b1a0fef3e14865b473da
Sigbjorn Finne <sof@galois.com>
2009-01-12 07:37:21 -0800

    include Lennart's latest mods

--- Text/JSON/Generic.hs
+++ Text/JSON/Generic.hs
@@ -2,7 +2,7 @@
 --------------------------------------------------------------------
 -- |
 -- Module    : Text.JSON.Generic
--- Copyright : (c) Lennart Augustsson, 2008
+-- Copyright : (c) Lennart Augustsson, 2008-2009
 -- License   : BSD3
 --
 -- Maintainer:  Sigbjorn Finne <sof@galois.com>
@@ -27,13 +27,18 @@ module Text.JSON.Generic
 
 import Control.Monad.State
 import Control.Monad.Identity
-import Data.Char(toLower, toUpper)
 import Data.Maybe
 import Text.JSON
 import Text.JSON.String ( runGetJSON )
 import Data.Generics
 import Data.Word
 import Data.Int
+
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.IntSet as I
+-- FIXME: The JSON library treats this specially, needs ext2Q
+-- import qualified Data.Map as M
 
 type T a = a -> JSValue
 
@@ -56,10 +61,14 @@ toJSON = toJSON_generic
          `extQ` (showJSON :: T Float)
          `extQ` (showJSON :: T Char)
          `extQ` (showJSON :: T String)
-         -- Some algebraic types have weird encodings.
+         -- Bool has a special encoding.
          `extQ` (showJSON :: T Bool)
          `extQ` (showJSON :: T ())
          `extQ` (showJSON :: T Ordering)
+         -- More special cases.
+         `extQ` (showJSON :: T I.IntSet)
+         `extQ` (showJSON :: T S.ByteString)
+         `extQ` (showJSON :: T L.ByteString)
   where
         -- Lists are simply coded as arrays.
         jList vs = JSArray $ map toJSON vs
@@ -89,7 +98,7 @@ toJSON_generic = generic
         encodeConstr c [] = JSString $ toJSString $ constrString c
         encodeConstr c as = jsObject [(constrString c, encodeArgs c as)]
 
-        constrString = mungeConstr . showConstr
+        constrString = showConstr
 
         encodeArgs c = encodeArgs' (constrFields c)
         encodeArgs' [] [j] = j
@@ -99,10 +108,6 @@ toJSON_generic = generic
         -- Skip leading '_' in field name so we can use keywords etc. as field names.
         mungeField ('_':cs) = cs
         mungeField cs = cs
-
-        -- Turn leading capital letter into lower case
-        mungeConstr (c:cs) = toLower c : cs
-        mungeConstr "" = ""
 
 	jsObject :: [(String, JSValue)] -> JSValue
         jsObject = JSObject . toJSObject
@@ -114,6 +119,7 @@ type F a = Result a
 fromJSON :: (Data a) => JSValue -> Result a
 fromJSON j = fromJSON_generic j
              `ext1R` jList
+	     --
              `extR` (value :: F Integer)
              `extR` (value :: F Int)
              `extR` (value :: F Word8)
@@ -128,9 +134,14 @@ fromJSON j = fromJSON_generic j
              `extR` (value :: F Float)
              `extR` (value :: F Char)
              `extR` (value :: F String)
+	     --
              `extR` (value :: F Bool)
              `extR` (value :: F ())
              `extR` (value :: F Ordering)
+	     --
+             `extR` (value :: F I.IntSet)
+             `extR` (value :: F S.ByteString)
+             `extR` (value :: F L.ByteString)
   where value :: (JSON a) => Result a
         value = readJSON j
 
@@ -153,25 +164,29 @@ fromJSON_generic j = generic
         getConstr t (JSObject o) | [(s, j')] <- fromJSObject o = do c <- readConstr' t s; return (c, j')
         getConstr t (JSString js) = do c <- readConstr' t (fromJSString js); return (c, JSNull) -- handle nullare constructor
         getConstr _ _ = Error "fromJSON: bad constructor encoding"
-        readConstr' t s = maybe (Error $ "fromJSON: unknown constructor: " ++ s ++ " " ++ show t) return .
-                          readConstr t . unmungeConstr $ s
+        readConstr' t s = 
+	  maybe (Error $ "fromJSON: unknown constructor: " ++ s ++ " " ++ show t) 
+	        return $ readConstr t s
 
-        decodeArgs c = decodeArgs' (numConstrArgs (resType generic) c) c
-        decodeArgs' 0 c JSNull = construct c []
-        decodeArgs' 1 c jd | null (constrFields c) = construct c [jd]
-        decodeArgs' n c (JSArray js) | (n::Int) > 1 && null (constrFields c) = construct c js
-        decodeArgs' _ c (JSObject o) | not (null (constrFields c)) = construct c (map snd $ fromJSObject o) -- FIXME: should use field names
-        decodeArgs' _ c jd = Error $ "fromJSON: bad decodeArgs data " ++ show (c, jd)
+        decodeArgs c = decodeArgs' (numConstrArgs (resType generic) c) c (constrFields c)
+        decodeArgs' 0 c  _       JSNull               = construct c []   -- nullary constructor
+        decodeArgs' 1 c []       jd                   = construct c [jd] -- unary constructor
+        decodeArgs' n c []       (JSArray js) | n > 1 = construct c js   -- no field names
+        -- FIXME? We could allow reading an array into a constructor with field names.
+        decodeArgs' _ c fs@(_:_) (JSObject o)         = selectFields (fromJSObject o) fs >>= construct c -- field names
+        decodeArgs' _ c _        jd                   = Error $ "fromJSON: bad decodeArgs data " ++ show (c, jd)
 
         -- Build the value by stepping through the list of subparts.
         construct c = evalStateT $ fromConstrM f c
           where f :: (Data a) => StateT [JSValue] Result a
                 f = do js <- get; case js of [] -> lift $ Error "construct: empty list"; j' : js' -> do put js'; lift $ fromJSON j'
 
-        unmungeConstr (c:cs) = toUpper c : cs
-        unmungeConstr "" = ""
+        -- Select the named fields from a JSON object.  FIXME? Should this use a map?
+        selectFields fjs = mapM sel
+          where sel f = maybe (Error $ "fromJSON: field does not exist " ++ f) Ok $ lookup f fjs
 
         -- Count how many arguments a constructor has.  The value x is used to determine what type the constructor returns.
+        numConstrArgs :: (Data a) => a -> Constr -> Int
         numConstrArgs x c = execState (fromConstrM f c `asTypeOf` return x) 0
           where f = do modify (+1); return undefined
 
